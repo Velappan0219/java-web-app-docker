c23a0488842a4464258f8f3ae7b3f0881359ccfa
velcoarumugam <51733722+Velappan0219@users.noreply.github.com>
2021-02-24 00:17:33 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -14,7 +14,7 @@ public class HelloWorldService {
 
 		logger.debug("getDesc() is executed!");
 
-		return "Maven + Spring MVC + Jenkins + Docker Example";
+		return "JENKINS CI/CD PIPELINE WITH DOCKER";
 
 	}
 
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Hello Docker Iam Velappan";
 		}else{
 			return "Hello " + name;
 		}
