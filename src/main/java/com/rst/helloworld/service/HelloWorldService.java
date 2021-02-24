f97c33f2a1421dfa031b2f62843462ebd6d89ed4
velcoarumugam <51733722+Velappan0219@users.noreply.github.com>
2021-02-25 03:12:05 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Iam nice docker life";
+			return "docker container with webhook";
 		}else{
 			return "Hello " + name;
 		}
