902dfe2b5925204678c7153ffecdbdf9b3bd978a
velcoarumugam <51733722+Velappan0219@users.noreply.github.com>
2021-02-26 15:16:21 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "jenkins github auto";
+			return "jenkins github auto candy";
 		}else{
 			return "Hello " + name;
 		}
